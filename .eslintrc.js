08df4a1246684f7bc2b6b55b57ed5500b0eec39e
Rustam Rudakov <rudakov.de@gmail.com>
2018-05-29 19:41:54 +0300

    added React support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,26 @@
 module.exports = {
-  extends: ['airbnb-base', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+
+    'airbnb-base',
+
+    'plugin:react/recommended',
+    'plugin:react-app/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+
   parserOptions: {
     ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
 
   rules: {
     'no-console': 0,
 
-    'no-plusplus': [
-      'error', { "allowForLoopAfterthoughts": true }
-    ],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
 
     'prettier/prettier': [
       'error',
@@ -20,7 +31,10 @@ module.exports = {
       },
     ],
 
+    'react-app/jsx-a11y/href-no-hash': 'off',
+
+    'class-methods-use-this': 'off',
   },
 
-  plugins: ['prettier'],
+  plugins: ['prettier', 'jsx-a11y', 'react'],
 };
